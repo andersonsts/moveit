18e7f90a5f186b504b7fae303aee217a58d257ae
andersonsts <andersonst.dev@gmail.com>
2021-09-17 20:45:36 -0300

    insert wrapper of countdown-provider

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,7 +4,14 @@ import { CountdownContext } from '../contexts/CountdownContext'
 import styles from '../styles/components/Countdown.module.css'
 
 export function Countdown () {
-  const { minutes, seconds, startCountdown, resetCountdown, isActive, hasFinished } = useContext(CountdownContext)
+  const { 
+    minutes,
+    seconds,
+    startCountdown,
+    resetCountdown,
+    isActive,
+    hasFinished 
+  } = useContext(CountdownContext)
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('') // padStart: 0 => 00...
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
