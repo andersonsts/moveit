cf6c5392af7db7be22dc74b6ca3b8e12ee7f8f55
andersonsts <andersonst.dev@gmail.com>
2021-09-18 13:53:12 -0300

    add button at login page

--- src/components/LoginInput.tsx
+++ src/components/LoginInput.tsx
@@ -1,11 +1,28 @@
 /* eslint-disable */
+import { useState } from 'react'
+
 import styles from 'styles/components/LoginInput.module.css'
 
-export function LoginInput () {
+interface LoginInput {
+  onClick: () => void;
+}
+
+export function LoginInput ({ onclick }) {
+  const [value, setValue] = useState('')
+
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Digite seu username" />
-      <button type="button" onClick={() => null}>
+      <input 
+        type="text" 
+        value={value}
+        placeholder="Digite seu username"
+        onChange={({ target }) => setValue(target.value)} 
+      />
+      <button 
+        type="button" 
+        onClick={onclick} 
+        className={!!value ? styles.filledButton : styles.normalButton}
+      >
         <img src="icons/arrow.svg" alt="Arrow icon" />
       </button>
     </div>
