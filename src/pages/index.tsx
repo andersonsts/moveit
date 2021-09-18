84f3e44c875d66514073ddb5b67b3ce18727af3c
andersonsts <andersonst.dev@gmail.com>
2021-09-17 23:46:30 -0300

    insert root path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,35 @@
 import Head from 'next/head'
 
-import { CompletedChallenges } from "../components/CompletedChallenges";
-import { Countdown } from "../components/Countdown";
-import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from '../components/Profile';
-import { ChallengeBox } from "../components/ChallengeBox";
-import { CountdownProvider } from '../contexts/CountdownContext';
+import { CompletedChallenges } from 'components/CompletedChallenges'
+import { Countdown } from "components/Countdown";
+import { ExperienceBar } from "components/ExperienceBar";
+import { Profile } from 'components/Profile';
+import { ChallengeBox } from "components/ChallengeBox";
+import { CountdownProvider } from 'contexts/CountdownContext';
 
-import styles from '../styles/pages/Home.module.css'
+import styles from 'styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
 
-      <ExperienceBar />
+        <ExperienceBar />
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
   )
 }
