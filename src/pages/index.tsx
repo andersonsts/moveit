325fff22d0dc4cc7acaba7df138a5b5090ac20fb
andersonsts <andersonst.dev@gmail.com>
2021-09-18 00:43:42 -0300

    save infos on cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 
 import { CompletedChallenges } from 'components/CompletedChallenges'
 import { Countdown } from "components/Countdown";
@@ -7,10 +8,19 @@ import { Profile } from 'components/Profile';
 import { ChallengeBox } from "components/ChallengeBox";
 import { CountdownProvider } from 'contexts/CountdownContext';
 
+import { ChallengesProvider } from 'contexts/ChallengesContext'
+
 import styles from 'styles/pages/Home.module.css'
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
+    <ChallengesProvider {...props}>
       <div className={styles.container}>
         <Head>
           <title>Início | move.it</title>
@@ -31,5 +41,18 @@ export default function Home() {
           </section>
         </CountdownProvider>
       </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { level, currentExperience, challengesCompleted } = context.req.cookies
+  
+  return {
+    props: { 
+      level: Number(level), 
+      currentExperience: Number(currentExperience), 
+      challengesCompleted: Number(challengesCompleted), 
+    }
+  }
 }
