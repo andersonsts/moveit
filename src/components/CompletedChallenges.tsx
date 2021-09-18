045c90ccaa2911a93f70cde4605aeb46b66d4d0f
andersonsts <andersonst.dev@gmail.com>
2021-09-17 23:57:08 -0300

    add padstart on challenges completed

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -7,10 +7,12 @@ import styles from 'styles/components/CompletedChallenges.module.css';
 export function CompletedChallenges () {
   const { challengesCompleted } = useContext(ChallengesContext)
   
+  const formattedChallengesCompleted = String(challengesCompleted).padStart(2, '0').split('')
+
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>{challengesCompleted}</span>
+      <span>{formattedChallengesCompleted}</span>
     </div>
   )
 }
