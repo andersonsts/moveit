1c503c1aac8a04b15c19030c703fa6a2f7520820
andersonsts <andersonst.dev@gmail.com>
2021-09-18 19:05:46 -0300

    add share to twitter button

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -18,6 +18,26 @@ export function LevelUpModal() {
         <button type="button" onClick={closeLevelUpModal}>
           <img src="/icons/close.svg" alt="Close modal" />
         </button>
+
+        <button type="button" style={{ 
+          width: '100%',
+          right: '0px',
+          bottom: '0px',
+          top: '308px',
+          color: '#2AA9E0',
+          fontSize: '20px', 
+          height: '80px',
+          fontWeight: 600,
+          lineHeight: '24px',
+          borderRadius: '0px 0px 5px 5px',
+          background: '#F5FCFF',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          Compartilhar no Twitter
+          <img src="icons/twitter.svg" alt="Twitter" style={{ marginLeft: '16px' }} />
+        </button>
       </div>
     </div>
   )
