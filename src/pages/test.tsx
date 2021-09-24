025d25eb1b0d90fcdd1e9949e75f7d17728f3ec0
andersonsts <andersonst.dev@gmail.com>
2021-09-23 23:33:23 -0300

    tests with responsiviness

---
+++ src/pages/test.tsx
@@ -0,0 +1,17 @@
+import styles from 'styles/test.module.css'
+
+export default function Test() {
+  return (
+    <div className={styles.container}>
+      <div>
+        OK
+      </div>
+      <div>
+        OK
+      </div>
+      <div>
+        OK
+      </div>
+    </div>
+  )
+}
