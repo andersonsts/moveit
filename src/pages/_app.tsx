8ff335e7106c382c2df909dc4976139f333fdd4c
andersonsts <andersonst.dev@gmail.com>
2021-10-05 21:30:03 -0300

    add ngprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,11 @@
+import Router from 'next/router';
 import 'styles/global.css'
+import NProgress from 'nprogress'; 
+import 'nprogress/nprogress.css'; 
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());  
 
 function MyApp({ Component, pageProps }) {
   return (
