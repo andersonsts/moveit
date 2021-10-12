c497b996f88bceb45d80ec3512d8582204d40302
andersonsts <andersonst.dev@gmail.com>
2021-10-12 18:00:41 -0300

    add open graph to test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,16 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+          {/* Twitter */}
+          <meta name="twitter:card" content="summary" key="twcard" />
+          <meta name="twitter:creator" content="Twitter Creator" key="twhandle" />
+
+          {/* Open Graph */}
+          <meta property="og:url" content={window.location.pathname} key="ogurl" />
+          <meta property="og:image" content="favicon.png" key="ogimage" />
+          <meta property="og:site_name" content="Move.it" key="ogsitename" />
+          <meta property="og:title" content="Move.it" key="ogtitle" />
+          <meta property="og:description" content="Move.it" key="ogdesc" />
         </Head>
         <body>
           <Main />
